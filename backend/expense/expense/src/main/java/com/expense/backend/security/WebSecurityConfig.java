44161625c239fe90a56441bd9bdcae0824c1c2b1
aashritareddy28 <aashritareddy28@gmail.com>
2026-04-03 13:50:50 +0530

    Update WebSecurityConfig.java

--- backend/expense/expense/src/main/java/com/expense/backend/security/WebSecurityConfig.java
+++ backend/expense/expense/src/main/java/com/expense/backend/security/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig {
             .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
 
         http.authenticationProvider(authenticationProvider());
